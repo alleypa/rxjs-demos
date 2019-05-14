7c5cc7a8beeff287f6372a0be32f3d3652fc0309
Olu Alli <olu.alli@hmcts.net>
2019-05-14 11:53:03 +0100

    1 - What are streams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { count } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,27 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+    document.addEventListener('click', evt => {  // mulit value streams that can never complete
+      console.log(evt);
+    });
+
+    let counter = 0;
+    setInterval(() => {  // mulit value streams
+      console.log(counter);
+      counter++;
+    }, 1000);
+
+    setTimeout(() => { // A type of stream that will complete
+      console.log('finished...');
+
+    }, 3000);
+  }
 }
+
+
+/**
+ * Streams are collection of values emitted over time
+ * and can either complete or not
+ *
+ */
